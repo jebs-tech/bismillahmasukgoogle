dd0625aeb1a21eadb3a67e939d649b9e0f3e0f65
jebs-tech <bungasaragih688@gmail.com>
2025-10-26 20:43:40 +0700

    perbaikan

--- ServeTix/urls.py
+++ ServeTix/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
+
 # Di ServeTix/urls.py
 
 # Di ServeTix/urls.py
@@ -20,6 +21,7 @@ urlpatterns = [
     path('vouchers/', include('voucher.urls', namespace='voucher')),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
